8a093e1b7021dbfb06b2091584bfa52c8b5c4264
nzlatanovic <nemanjazlatanovicc@gmail.com>
2026-09-23 01:32:39 +0200

    Dan 2, nastavak ucenja tensora i dodata f-ja def describe_tensor(x)

--- pocetak/main.py
+++ pocetak/main.py
@@ -2,6 +2,22 @@ import torch
 
 device = torch.device("cuda")
 
+def describe_tensor(x):
+    print(f"--- Tensor Pregled ---")
+    print(f"Vrednosti:\n{x}")
+    print(f"Dimenzije: {x.shape}")
+    print(f"Tip podataka: {x.dtype}")
+    print(f"Gde se nalazi: {x.device}")
+    print(f"Broj dimenzija: {x.ndim}")
+    print(f"Broj elemenata: {x.numel()}")
+    print(f"Prosek: {x.mean()}")
+    print(f"Najmanji element: {x.min()}")
+    print(f"Najveci element: {x.max()}")
+    print(f"----------------------\n")
+
+
+
+
 """
 x = torch.tensor([
     [1.0, 2.0],
@@ -14,7 +30,7 @@ w = torch.tensor([
 ])
 
 
-y = x @ w
+y = x @ w #Ovo je matricno mnozenje
 
 print(y)
 print(y.shape)
@@ -32,22 +48,45 @@ x = x.to("cuda")
 
 print(x.device)"""
 
-x = torch.tensor([[1.,2.,3.],
-                  [4.,5.,6.],
-                  [7.,8.,9.]], device=device)
 
-"""print(x[1,:])
+A = torch.tensor([[1.,2.,3.],
+                  [4.,5.,6.]], device=device)
+
+
+
+B = torch.tensor([[9.,8.],
+                 [6.,5.],
+                 [3.,2.]], device=device)
+
+C = torch.tensor([[1.,2.],
+                  [3.,4.]], device=device)
+x = torch.tensor([10.,20.,30.], device=device)
+
+D = torch.randn(32, 10, 64) #batch, sequence, embedding (ovo se koristi u Transformerima)
+E = torch.randn(64, 128)
+# A @ B - Ovo je matricno mnozenje 2x3 @ 3x2 = 2x2, takodje B @ A je 3x2 @ 2x3 = 3x3
+# A.T ovo je transponovanje matrice i ono menja broj redova i kolona (vrednosti zamene mesta suprotno od glavne dijagonale tj 1.2 predje na 2.1 i vice versa)
+F = D @ E 
+
+describe_tensor(C)
+
+
+
+"""
+print(B.T+x)
+print(A*B)
+print(A@B)
+print(x[:, 1])
+#print(torch.mean(x[1:3]))
+ravan_niz=x.flatten()
+print(ravan_niz[3:7])
+print(x[1,:])
 print(x[:,1])
-print(x[2,1])"""
-
-y = torch.tensor([[9.,8.,7.],
-                 [6.,5.,4.],
-                 [3.,2.,1.]], device=device)
-
+print(x[2,1])
 print(x+y)
 print(x*y)
 print(x @ y)
-"""print(x)
+print(x)
 print(x.shape)
 print(x.dtype)
 print(x.device)
